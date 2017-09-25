ccbcc461de465b94d8af0148af86f87e9ea42b64
SW_Wind <2670529647@qq.com>
2017-09-25 21:48:23 +0800

    codeforces

--- A.cpp
+++ A.cpp
@@ -3,32 +3,51 @@
 #define ll long long
 using namespace std;
 inline int read(){
-	int x=0,f=1;char ch=getchar();
+	int x=0,f=1; char ch=getchar();
 	while(ch>'9'||ch<'0')ch=='-'&&(f=0)||(ch=getchar());
 	while(ch<='9'&&ch>='0')x=(x<<3)+(x<<1)+ch-'0',ch=getchar();
 	return f?x:-x;
 }
-char ch[N];
-int a[N], cnt;
-int main(int argc, char const *argv[]){
-	int n = read(), sum = 0;
-	scanf("%s", ch+1); n = strlen(ch+1);
-	if(n == 1 && ch[1] == '0') return puts("0\n0\n0");
-	for(int i = 1; i <= n; i++){
-		sum += ch[i]-'0';
-		printf("%c%c", ch[i], i == n ? '\n' : '+');
+int f[125][2025], last[125][2025], vis[125][2025];
+int sta[125], top;
+int n = read(), ans, mxt, num;
+struct node{
+	int thisisalongvarname, thisisanotherlongvarname, thisisthenonono, num;
+	friend bool operator < (const node &aa, const node &bb) {
+		return aa.thisisanotherlongvarname < bb.thisisanotherlongvarname;
 	}
-	for(;sum;sum/=10)a[++cnt]=sum%10;
-	sum = 0; if(!cnt) cnt++;
-	for(int i = cnt; i; i--){
-		sum += a[i];
-		printf("%d%c", a[i], i == 1?'\n':'+');
-	} cnt = 0;
-	for(;sum;sum/=10)a[++cnt]=sum%10;
-	sum = 0; if(!cnt) cnt++;
-	for(int i = cnt; i; i--){
-		sum += a[i];
-		printf("%d%c", a[i], i == 1?'\n':'+');
-	} cnt = 0;
+}miku[125];
+int main(){
+	for (int i = 1; i <= n; i++){
+		miku[i].thisisalongvarname = read(); miku[i].thisisanotherlongvarname = read(); miku[i].thisisthenonono = read();
+		miku[i].num = i;
+		mxt = max(mxt,miku[i].thisisanotherlongvarname);
+	}
+	sort(miku+1, miku+n+1);
+	for (int i = 1; i <= n; i++){
+		for (int j = miku[i].thisisalongvarname; j < miku[i].thisisanotherlongvarname; j++){
+			if(f[i][j] <= f[i-1][j-miku[i].thisisalongvarname]+miku[i].thisisthenonono){
+				f[i][j] = f[i-1][j-miku[i].thisisalongvarname]+miku[i].thisisthenonono;
+				last[i][j] = j-miku[i].thisisalongvarname;
+				vis[i][j] = 1;
+			}
+		}
+		for (int j = 0; j < mxt; j++)
+			if(f[i][j] <= f[i-1][j]) {
+				f[i][j] = f[i-1][j];
+				last[i][j] = j;
+				vis[i][j] = 0;
+			}
+	}
+	for (int i = 0; i < mxt; i++)
+		if(ans < f[n][i])
+			ans = f[n][num = i];
+	printf("%d\n", ans);
+	for (int i = n; i; i--){
+		if (vis[i][num])
+			sta[++top] = miku[i].num;
+		num = last[i][num];}
+	printf("%d\n", top);
+	while (top) printf("%d ",sta[top--]);
 	return 0;
 }
