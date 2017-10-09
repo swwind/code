6172ddfdf6c47287e9318ed5365da54ceb23db95
SW_Wind <2670529647@qq.com>
2017-10-09 18:41:12 +0800

    what happend?

---
+++ luogu/luogu1150.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define N 100020
+#define ll long long
+using namespace std;
+inline int read(){
+	int x=0,f=1;char ch=getchar();
+	while(ch>'9'||ch<'0')ch=='-'&&(f=0)||(ch=getchar());
+	while(ch<='9'&&ch>='0')x=(x<<3)+(x<<1)+ch-'0',ch=getchar();
+	return f?x:-x;
+}
+int main(int argc, char const *argv[]) {
+	int n = read(), k = read();
+	int ans = n;
+	while (n >= k) {
+		ans += n/k;
+		n = n%k+n/k;
+	}
+	printf("%d\n", ans);
+	return 0;
+}
