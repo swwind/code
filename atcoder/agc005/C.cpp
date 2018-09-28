64f50d4222c5b5a8af92345860c7353477c6fe3c
swwind <swwind233@gmail.com>
2018-09-28 14:02:55 +0800

    update

--- atcoder/agc005/C.cpp
+++ atcoder/agc005/C.cpp
@@ -8,12 +8,34 @@ inline int read(){
   while(ch<='9'&&ch>='0')x=(x<<3)+(x<<1)+ch-'0',ch=getchar();
   return f?x:-x;
 }
+int cnt[N], a[N];
 int main(int argc, char const *argv[]) {
-  
+  int n = read();
+  int r = 1 << 30, d = 0;
+  for (int i = 1; i <= n; ++ i){
+    a[i] = read();
+    cnt[a[i]] ++;
+    r = min(a[i], r);
+    d = max(a[i], d);
+  }
+  if (r != (d + 1) / 2 || cnt[r] > 1 + (d & 1)) {
+    return puts("Impossible"), 0;
+  }
+  for (int i = r + 1; i <= d; ++ i) {
+    if (cnt[i] < 2) {
+      return puts("Impossible"), 0;
+    }
+  }
+  puts("Possible");
   return 0;
 }
 /*
 显然 max{a[i]} 即为树的直径。
 
 显然 [直径/2,直径] 必须都存在。
+
+除半径外所有值都应该>=2
+
+半径分情况讨论
+
 */
