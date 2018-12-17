e0a874dac1a52b8137f8a3fed292a77f4ec3f924
swwind <swwind233@gmail.com>
2018-12-17 10:34:27 +0800

    wtf

--- codeforces/915G.cpp
+++ codeforces/915G.cpp
@@ -8,7 +8,24 @@ inline int read(){
   while(ch<='9'&&ch>='0')x=(x<<3)+(x<<1)+ch-'0',ch=getchar();
   return f?x:-x;
 }
+
+ll fast_pow(ll x, ll y) {
+  ll z = 1;
+  for (; y; y >>= 1, x = x * x % mod) {
+    if (y & 1) {
+      z = z * x % mod;
+    }
+  }
+  return z;
+}
+
+ll filter_mu(const ll max_size) {
+  for (int i = 2; i <= max_size; ++ i) {
+    if (!mark[i]) pri[++ cnt] = i;
+  }
+}
+
 int main(int argc, char const *argv[]) {
-  cout << 233_233_233 << endl;
+  
   return 0;
 }
